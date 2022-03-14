a9b53ef5c5b55c5577983c6179c049126a395b4d
Maribel Montes <maribel.doris@gmail.com>
2022-03-14 13:59:51 -0500

    added the seed.js file

---
+++ server/seeders/seed.js
@@ -0,0 +1,25 @@
+const db = require('../config/connection');
+const { User, Bug } = require('../models');
+// seed json files
+const userSeeds = require('./userSeeds.json');
+const bugSeeds = require('./bugSeeds.json');
+
+db.once('open', async () => {
+        await User.deleteMany({});
+        await Bug.deleteMany({});
+       
+    // bulk create each model
+    const users = await User.insertMany(userSeeds);
+    const bugs = await Bug.insertMany(bugSeeds);
+
+    for (newbug of bugs) {
+        // randomly add each class to a school
+        const tempuser = users[Math.floor(Math.random() * users.length)];
+        tempuser.bugs.push(newbug._id);
+        await tempuser.save();
+    }
+
+    // all done
+    console.log('all done!');
+    process.exit(0);
+});
