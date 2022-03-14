4bf5253fdc5936ab0e503a133e17ea0438cac816
George Geils <95538913+ggeils@users.noreply.github.com>
2022-03-14 12:01:12 -0400

    Base bug report form complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import Login from './components/Login'
-import Report from './components/Report'
 
 const App = () => {
   return (
     <div>
-      <Report />
+      <Login />
     </div>
   )
 }
