5d224d8afba357c5955ffc1b2cadc453bb26a5bd
Maribel Montes <maribel.doris@gmail.com>
2022-03-14 13:48:12 -0500

    added the bug.js model, about bugs shema

---
+++ server/models/Bug.js
@@ -0,0 +1,64 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+/* bugName: string
+ * productName: string
+ * date: Date.now
+ * severity: string ( low || medium || High)
+ * description: string
+ * reproduction: string
+ * status: string (Open || Closed) 
+ *  default: open
+ */
+const bugsSchema = new Schema({
+    bugName: {
+        type: String,
+        required: true,
+        maxlength: 50,
+        minlength: 4,
+    },
+    productName: {
+        type: String,
+        required: true,
+        maxlength: 50,
+        minlength: 4,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+    severity: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength:280,
+    },
+    reproduction: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength:280,
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'open',
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+//initialize the BUG schema model
+const Bug = model('Bug', bugsSchema);
+
+module.exports = Bug;
