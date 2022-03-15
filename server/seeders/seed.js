5a2e331abc356049900cd4de2f4b74b72604198a
Maribel Montes <maribel.doris@gmail.com>
2022-03-14 19:27:03 -0500

    changed the insertMany to create in the seed.js

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -5,12 +5,12 @@ const userSeeds = require('./userSeeds.json');
 const bugSeeds = require('./bugSeeds.json');
 
 db.once('open', async () => {
-        await User.deleteMany({});
-        await Bug.deleteMany({});
+    await User.deleteMany({});
+    await Bug.deleteMany({});
        
     // bulk create each model
-    const users = await User.insertMany(userSeeds);
-    const bugs = await Bug.insertMany(bugSeeds);
+    const users = await User.create(userSeeds);
+    const bugs = await Bug.create(bugSeeds);
 
     for (newbug of bugs) {
         // randomly add each class to a school
