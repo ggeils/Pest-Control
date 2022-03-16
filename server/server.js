87755f3735066133ecf5ceb78b9b273d97fd647e
Maribel Montes <maribel.doris@gmail.com>
2022-03-16 15:29:37 -0500

    fixed issue on the server to merge the files

--- server/server.js
+++ server/server.js
@@ -18,11 +18,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 
