6fb675f38941fd069b5ed82e389ad898d8bbbee7
Maribel Montes <maribel.doris@gmail.com>
2022-03-16 10:41:31 -0500

    added the update user profile schema

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,7 +17,7 @@ const resolvers = {
             return Bug.find().sort({ date: -1 });
         },
         // By adding context to our query, we can retrieve the logged in user without specifically searching for them
-        me: async (parent, args, context) => {
+        userLoged: async (parent, args, context) => {
             if (context.user) {
                 return User.findOne({ _id: context.user._id }).populate('bugs');
             }
@@ -42,7 +42,18 @@ const resolvers = {
             
             const token = signToken(user);
             return { token, user };
-          },
+        },
+        // Update user profile - update all user profile info
+        updateprofile: async (parent, args, context) => {
+            // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+            if(context.user){
+                return User.findByIdAndUpdate( context.user._id, args, { new: true } );
+            }
+            // If user attempts to execute this mutation and isn't logged in, throw an error
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        
+
     },
 };
 
